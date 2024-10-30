614624d8670ad4c924ddec1ad52691273e35e244
LevineoS <levisantosbr5@gmail.com>
2024-10-29 21:22:01 -0300

    Exercicios 1-12 lista 1

---
+++ Exercicios/Ex 5/JS/ex5.js
@@ -0,0 +1,6 @@
+var m = parseFloat(prompt("Informe quantos metros serao: "));
+var cm = parseFloat;
+
+cm = m * 100
+
+console.log("Isso em centimetros e: ", cm);
---
+++ Exercicios/Ex 7/JS/ex7.js
@@ -0,0 +1,5 @@
+var a = parseFloat(prompt("Insira a altura do quadrado: "));
+
+area = (a*a) * 2
+
+console.log("Sua area em dobro e igual: ", area);
